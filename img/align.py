7a2e883d422239593fd5ab9c1e8790d492e455da
jaime <jaime@astro.su.se>
2019-10-21 21:05:45 +0200

    created align.py to compute alignment between 2D images

---
+++ img/align.py
@@ -0,0 +1,38 @@
+
+def align(a, b):        
+    """
+    Function align computes the shifts (dy,dx) needed to align image b into a
+    Arguments:
+       a: 2D numpy array of dimensions (ny, nx)
+       b: 2D numpy array of dimensions (ny, nx)
+    Returns:
+       A tuple with the (y,x) shifts in pixel units.
+    
+    Example:
+       a = readimage('bla.fits')
+       b = readimage('ble.fits')
+       dy,dx = align(a,b)
+
+    Ported from IDL by J. de la Cruz Rodriguez (ISP-SU 2015)
+    """
+    if(a.shape[0] != b.shape[0] or a.shape[1] != b.shape[1]):
+        print("align: ERROR, both images must have the same size")
+        return(0.0,0.0)
+    
+    fa = np.fft.fft2(a)
+    fb = np.fft.fft2(b)
+
+    cc = np.roll(np.roll(np.real(np.fft.ifft2(fa.conjugate() * fb)), -int(fa.shape[0]//2), axis=0), -int(fa.shape[1]//2), axis=1)
+    
+    mm = np.argmax(cc)
+    xy = ( mm // fa.shape[1], mm % fa.shape[1])
+
+    cc = cc[xy[0]-1:xy[0]+2, xy[1]-1:xy[1]+2]
+    y = 2.0*cc[1,1]
+    x = (cc[1,0]-cc[1,2])/(cc[1,2]+cc[1,0]-y)*0.5
+    y = (cc[0,1]-cc[2,1])/(cc[2,1]+cc[0,1]-y)*0.5
+
+    x += xy[1] - fa.shape[1]//2
+    y += xy[0] - fa.shape[0]//2
+
+    return(y,x)
