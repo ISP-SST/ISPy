cf6ba4cf91880c60adf5526f7c3711bd823be802
Gregal Vissers <gregal.vissers@astro.su.se>
2019-10-31 10:13:09 +0100

    Add function to return viewing angle

---
+++ util/obs.py
@@ -0,0 +1,35 @@
+import numpy as np
+from datetime import datetime
+
+import sunpy.coordinates.sun as sun
+
+def viewangle(xy, date=None):
+    """
+    Return the viewing angle theta (and mu=cos(theta)) given a set of solar
+    (X,Y) coordinates in arcsec
+
+    Arguments:
+        xy: 2-element list or array with the (X,Y) coordinate values (in arcsec)
+
+    Keyword arguments:
+        date: date for which to get the viewing angle (default: today's date)
+
+    Returns:
+        2-element list with [theta, mu]. Theta is given in degrees.
+
+    Example:
+        result = viewangle([240,-380], date='2017-08-02')
+
+    Author:
+        Gregal Vissers (ISP/SU 2019)
+    """
+
+    if date is None:
+        date = datetime.now().date().strftime('%Y-%m-%d')
+
+    r_sun = sun.angular_radius(date).value
+    rho = np.sqrt(xy[0]**2 + xy[1]**2)
+    mu = np.sqrt(1 - (rho/r_sun)**2)
+    theta = np.degrees(np.arccos(mu))
+
+    return [theta, mu]
