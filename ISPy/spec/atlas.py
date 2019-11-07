ef684d0b4bb490ea6ea4f2d272788fe0435d31de
Gregal Vissers <gregal.vissers@astro.su.se>
2019-11-07 11:55:08 +0530

    Merge cgs-SI conversion methods into single method

--- ISPy/spec/atlas.py
+++ ISPy/spec/atlas.py
@@ -25,33 +25,56 @@ class atlas:
     def __init__(self):
         # Check dir where this class is stored
         this_dir, this_filename = os.path.split(__file__)
-        DATA_PATH = os.path.join(this_dir, "../data/fts_disk_center.idlsave")
+        DATA_PATH = os.path.join(this_dir, "../data/fts_disk_center_SI.idlsave")
 
         # Load data file
         fts = idl.readsav(DATA_PATH)
-        self.cont = fts["ftscnt"]
-        self.spec = fts["ftsint"]
-        self.wave = fts["ftswav"]
+        self.cont = np.copy(fts["ftscnt_SI"])
+        self.spec = np.copy(fts["ftsint_SI"])
+        self.wave = np.copy(fts["ftswav"])
+        self.usys = 'si_inu' # J/s/m^2/sr/Hz
+        self.sunit = u.J / (u.s * u.m**2 * u.steradian * u.Hz)
+        self.wunit = u.Angstrom
 
 
-    def tocgs(self, w, s):
-        clight = const.c.to('cm/s').value #speed of light [cm/s]
-        joule_2_erg = u.J.to('erg')
-        aa_to_cm = u.Angstrom.to('cm')
-        s *= joule_2_erg/aa_to_cm # from Watt /(cm2 ster AA) to erg/(s cm2 ster cm)
-        s *= (w*aa_to_cm)**2/clight   # to erg/
-        return s
+    def to(self, usys_to, perHz=True):
+        usys_from = self.usys.lower()
 
-    def tosi(self, wav, s):
-        clight = const.c.value #speed of light [m/s]                                  
-        aa_to_m = u.Angstrom.to('m')
-        cm_to_m = u.cm.to('m')
-        s /= cm_to_m**2 * aa_to_m # from from Watt /(s cm2 ster AA) to Watt/(s m2 ster m) 
-        s *= (wav*aa_to_m)**2 / clight # to Watt/(s m2 Hz ster)
-        return s
-    
-    def get(self, w0, w1, cgs = False, si = False, nograv = False):
-        idx = (np.where((self.wav >= w0) & (self.wav <= w1)))[0]
+        # Determine SI <-> cgs conversion
+        if usys_to.lower() == 'cgs' and usys_from[:2] == 'si':
+            conversion = u.J.to('erg') / (u.m.to('cm')**2)
+            self.sunit *= u.m**2 / u.J * u.erg / u.cm**2
+        elif usys_to.lower() == 'si' and usys_from[:3] == 'cgs':
+            conversion = u.erg.to('J') / (u.cm.to('m')**2)
+            self.sunit *= u.cm**2 / u.erg * u.J / u.m**2
+        else:
+            conversion = 1.
+
+        # Apply I_lambda (per AA) <-> I_nu (per Hz) if need be
+        lambda_to_nu = (self.wave*u.Angstrom.to('m'))**2 / const.c.value
+        if (perHz == False and usys_from[-3:] != 'inu') or \
+            (perHz == True and usys_from[-3:] == 'inu'):
+            # no change to conversion factor
+            if perHz == True:
+                ext = '_inu'
+            else:
+                ext = '_ilambda'
+        elif (perHz == False and usys_from[-3:] == 'inu'):
+            conversion /= lambda_to_nu
+            self.sunit *= u.Hz / u.Angstrom
+            ext = '_ilambda'
+        else:
+            conversion *= lambda_to_nu
+            self.sunit *= u.Angstrom / u.Hz
+            ext = '_inu'
+
+        # Apply conversion and update current unit system
+        self.spec *= conversion
+        self.cont *= conversion
+        self.usys = usys_to + ext
+
+    def get(self, w0, w1, cgs = False, si = False, nograv = False, perHz=True):
+        idx = (np.where((self.wave >= w0) & (self.wave <= w1)))[0]
 
         wave = np.copy(self.wave[idx[0]:idx[-1]])
         spec = np.copy(self.spec[idx[0]:idx[-1]])
@@ -62,13 +85,11 @@ class atlas:
 
         # convert to CGS units
         if(cgs):
-            spec = self.tocgs(wave, spec)
-            cont = self.tocgs(wave, cont)
+            self.to('cgs', perHz=perHz)
 
         # convert to IS units
         elif(si):
-            spec = self.tosi(wave, spec)
-            cont = self.tosi(wave, cont)
+            self.to('si', perHz=perHz)
 
         # Normalize by the continuum (default)
         else:
