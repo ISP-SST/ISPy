b6babf867b57e00df365f360f63cb7b1c189aca6
Carlos José Díaz Baso <carloskdb@gmail.com>
2021-11-25 21:17:24 +0100

    Update obs.py
    Obtain an approximate value even if sunpy is not installed.

--- ISPy/util/obs.py
+++ ISPy/util/obs.py
@@ -3,12 +3,18 @@ from datetime import datetime
 
 from astropy.coordinates import SkyCoord
 import astropy.units as u
-from sunpy.coordinates import frames
-import sunpy.coordinates.sun as sun
+try:
+    from sunpy.coordinates import frames
+    import sunpy.coordinates.sun as sun
+    sunpylib = True
+except:
+    sunpylib = False
+    print("[ISPy INFO] Install the sunpy package for an accurate implementation.")
+
 
 def viewangle(xy, date=None, heliographic=False):
     """
-    Return the viewing angle theta (and mu=cos(theta)) given a set of solar
+    Return the viewing angle theta (and mu = cos(theta)) given a set of solar
     coordinates in the helioprojective or heliographic Stonyhurst system
 
     Parameters
@@ -29,8 +35,9 @@ def viewangle(xy, date=None, heliographic=False):
 
     Example
     -------
-    >>> result = viewangle([240,-380], date='2017-08-02')
-    >>> result = viewangle([30,-50], date='2019-01-12', heliographic=True) # S50 W30
+    >>> from ISPy.util import obs
+    >>> result = obs.viewangle([240,-380], date='2017-08-02')
+    >>> result = obs.viewangle([30,-50], date='2019-01-12', heliographic=True) # S50 W30
 
     :Author:
         Gregal Vissers (ISP/SU 2019)
@@ -39,14 +46,18 @@ def viewangle(xy, date=None, heliographic=False):
     if date is None:
         date = datetime.now().date().strftime('%Y-%m-%d')
 
-    # Convert to helioprojective if input is heliographic Stonyhurst
-    if heliographic is True:
-        c = SkyCoord(xy[0]*u.deg, xy[1]*u.deg,
-                frame=frames.HeliographicStonyhurst, obstime=date)
-        xy_hpc = c.transform_to(frames.Helioprojective)
-        xy = [xy_hpc.Tx.value, xy_hpc.Ty.value]
+    if sunpylib is True:
+        # Convert to helioprojective if input is heliographic Stonyhurst
+        if heliographic is True:
+            c = SkyCoord(xy[0]*u.deg, xy[1]*u.deg,
+                    frame=frames.HeliographicStonyhurst, obstime=date)
+            xy_hpc = c.transform_to(frames.Helioprojective)
+            xy = [xy_hpc.Tx.value, xy_hpc.Ty.value]
 
-    r_sun = sun.angular_radius(date).value
+        r_sun = sun.angular_radius(date).value
+    else:
+        r_sun = 960.469 # Average radius in arcsec
+    
     rho = np.sqrt(xy[0]**2 + xy[1]**2)
     if (rho > r_sun):
         raise ValueError("viewangle: coordinates ({0},{1}) are not on the solar "
