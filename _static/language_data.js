a9f020cd2549ce2c4a8404529a170a330d511790
AlexPietrow <AlexPietrow@users.noreply.github.com>
2022-05-30 11:39:48 +0000

    deploy: a5dcae058cf31f3cde43e9ac297c3713b166c1c9

--- _static/language_data.js
+++ _static/language_data.js
@@ -10,7 +10,7 @@
  *
  */
 
-var stopwords = ["a","and","are","as","at","be","but","by","for","if","in","into","is","it","near","no","not","of","on","or","such","that","the","their","then","there","these","they","this","to","was","will","with"];
+var stopwords = ["a", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "near", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"];
 
 
 /* Non-minified version is copied as a separate JS file, is available */
